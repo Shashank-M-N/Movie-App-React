e4f1d5c64d2b8bd4f957ad7e81ccbbf9cf88531d
Shashank M N <mnshashank432@gmail.com>
2025-01-28 20:47:36 +0530

    updated app.jsx for deploying

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-import './css/App.css';
+import './css/App.css'
 import Favourite from './pages/Favourites';
-import Home from './pages/Home';
+import Home from './pages/Home'
 import NavBar from './components/NavBar';
 import { MovieProvider } from './contexts/MovieContext';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <MovieProvider>
-      <Router>
-        <NavBar />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/favourites" element={<Favourite />} />
-          </Routes>
-        </main>
-      </Router>
+      <NavBar />
+      <main className='main-content'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favourites" element={<Favourite />} />
+        </Routes>
+      </main>
     </MovieProvider>
-  );
+  )
 }
 
 export default App;
